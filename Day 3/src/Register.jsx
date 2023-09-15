3eafb73ea55774c65ea0b217d0b53f630a3c7dda
NoufalAnsari <120083214+NoufalAnsari@users.noreply.github.com>
2023-09-15 15:14:00 +0530

    Add files via upload

---
+++ Day 3/src/Register.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Register</h2>
+        <form className="register-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">User Name</label>
+        <input type="text" placeholder="Enter Your User Name" value={name} onChange={(e) => setName(e.target.value)} name="name" required />
+            <label htmlFor="email">Email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Enter Email"  id="email" name="email" required/>
+            <label htmlFor="password">Password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Enter Password" id="password" name="password" required/>
+        <button type="submit">Sign Up</button>
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+    </div>
+    )
+}
