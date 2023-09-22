9b4279f9e4538a24624fc2a36531b7f9dd7a342d
NoufalAnsari <120083214+NoufalAnsari@users.noreply.github.com>
2023-09-22 11:49:56 +0530

    Add files via upload

---
+++ Day 5/src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+     
+      <p>&copy; {new Date().getFullYear()} VoteEase Pvt.</p>
+    </footer>
+  );
+};
+
+export default Footer;
