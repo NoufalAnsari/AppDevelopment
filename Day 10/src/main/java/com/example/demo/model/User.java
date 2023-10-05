e80d28768d9618edd4878abfba98292902f6737a
NoufalAnsari <120083214+NoufalAnsari@users.noreply.github.com>
2023-10-05 15:45:17 +0530

    Add files via upload

---
+++ Day 10/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,79 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "voters")
+public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	@Column(name = "User_NAME")
+    private String username;
+	@Column(name = "User_EMAIL")
+    private String email;
+	@Column(name = "User_PWD")
+    private String password;
+
+    // Constructors
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // toString() method (optional, for debugging purposes)
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+}
+
